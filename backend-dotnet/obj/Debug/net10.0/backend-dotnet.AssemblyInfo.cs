87759d38c3ffdc5e09478d3f410b496295e74bd7
gmoncorvo <gabriel.moncorvoo@gmail.com>
2026-03-30 19:15:42 -0300

    Atualização do README com melhorias da API até o Dia 4

--- backend-dotnet/obj/Debug/net10.0/backend-dotnet.AssemblyInfo.cs
+++ backend-dotnet/obj/Debug/net10.0/backend-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c0e9e00351e90e040f4c41c793f519c3559109b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07cbecf216080253331b690c1a875bb4b52bb3c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
